b9259af11b1b243cce90e23b41f8e0c16371c1e5
Alex Rozsa <aorozsa@gmail.com>
2018-05-22 13:50:36 +1000

    Begining to create database & interface with it
    Began implementing the database and the data retrieval code in SearchResults.php

--- SearchResults.php
+++ SearchResults.php
@@ -19,21 +19,26 @@
     
     include 'navBar.php';
 
-    /*//How to access data from the DB
-    $pdo = new PDO('mysql:host=localhost;dbname=test', 'admin', 'secret!');
-    $stmt = $pdo->prepare('SELECT * FROM Customers WHERE firstname = :firstname');
-    $stmt->bindValue(':firstname', $_GET['firstname']);
-    $stmt->execute();*/
+    //How to access data from the DB
+    $pdo = new PDO('mysql:host=localhost;dbname=cab230_db', 'admin', 'secret!');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $stmt = $pdo->query('SELECT h.title, r.description' .
+                            'FROM hotspots h, reviews r;');
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
 
     echo '<div class="result_block">';
-    /*
+    
     foreach ($stmt as $hotspot) {
-        result($hotspot.title, $hotspot.description);
+        result($hotspot['h.title'], ($hotspot['r.description'] . '...'));
     }
-    */
+    /*
     result("Brisbane Square Library Wifi", "This is a really useful hotspot to have because...");
     result("City Botanic Gardens Wifi", "This is really, really convenient to have because...");
     result("Hamilton Library Wifi", "I can use this hotspot to check my emails and other...");
+    */
     echo '</div>';
 
     include 'map.php';
