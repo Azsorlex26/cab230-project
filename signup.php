30212229376159a2fa13c994153fd6739604e8f8
Alex Rozsa <aorozsa@gmail.com>
2018-05-26 20:47:28 +1000

    Began working on user registration and put in a new db file

--- signup.php
+++ signup.php
@@ -10,42 +10,49 @@
 <body>
 
     <?php 
-    function inputFeild($name, $title, $type, $placeholder) {
+    function inputField($name, $title, $type, $placeholder, $state) {
         echo "<label for=$name>";
         echo "<b>$title</b>";
         echo '</label><br>';
-        echo "<input type=$type placeholder=$placeholder name=$name required id=$name><br><br>";
+        if ($state) {
+            ?>
+            <select id="state" required id="state">
+                <option value="Queensland">Queensland</option>
+                <option value="NSW">NSW</option>
+                <option value="Victoria">Victoria</option>
+                <option value="ACT">ACT</option>
+                <option value="Tasmania">Tasmania</option>
+                <option value="South Australia">South Australia</option>
+                <option value="Northern Territory">Northern Territory</option>
+                <option value="Western Australia">Western Australia</option>
+            </select>
+            <br>
+            <br>
+            <?php
+        } else {
+            echo "<input type=$type placeholder=$placeholder name=$name required id=$name><br><br>";
+        }
     }
     
     include 'navBar.php';
 
-    echo '<form><div class="containerLogin"><h1>User Registration</h1><hr><br>';
-    inputFeild("fName", "First Name", "text", "Please enter your first name");
-    inputFeild("lName", "Last Name", "text", "Please enter your last name");
-    inputFeild("username", "Username", "text", "Please enter your Username");
-    inputFeild("email", "email", "text", "Please enter your email");
+    ?>
+    <form method="POST" action="signup.php">
+        <div class="containerLogin">
+        <h1>User Registration</h1><hr><br>
+    <?php
+    inputField("fName", "First Name", "text", "Please enter your first name", false);
+    inputField("lName", "Last Name", "text", "Please enter your last name", false);
+    inputField("username", "Username", "text", "Please enter your Username", false);
+    inputField("email", "email", "text", "Please enter your email", false);
+    inputField("state", "State", "", "", true);
 
-    echo '<label for="state">
-            <b>State</b>
-        </label>
-        <br>
-        <select id="state" required id="state">
-            <option value="Queensland">Queensland</option>
-            <option value="NSW">NSW</option>
-            <option value="Victoria">Victoria</option>
-            <option value="ACT">ACT</option>
-            <option value="Tasmania">Tasmania</option>
-            <option value="South Australia">South Australia</option>
-            <option value="Northern Territory">Northern Territory</option>
-            <option value="Western Australia">Western Australia</option>
-        </select>
-        <br>
-        <br>';
-
-    inputFeild("postCode", "Postcode", "text", "Please enter your postcode");
-    inputFeild("psw", "Password", "password", "Please enter your Password");
-    inputFeild("psw-repeat", "Re-enter Password", "password", "Please re-enter your Password");
+    inputField("postCode", "Postcode", "text", "Please enter your postcode", false);
+    inputField("psw", "Password", "password", "Please enter your Password", false);
+    inputField("psw-repeat", "Re-enter Password", "password", "Please re-enter your Password", false);
     echo '</div></form>';
+
+    
 
     include 'fixedfooter.php';
     ?>
