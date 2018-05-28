b58b95644c483a37684d42767428d4edece3836a
Alex Rozsa <aorozsa@gmail.com>
2018-05-28 13:27:10 +1000

    changed navbar item reference from search to index

--- navBar.php
+++ navBar.php
@@ -4,7 +4,7 @@
         function createItem($class, $page, $text) {
             echo "<li><a class=$class href=$page>$text</a></li>";
         }
-        createItem("left", "search.php", "Search Page");
+        createItem("left", "index.php", "Search Page");
         createItem("right", "signup.php", "Sign Up");
         session_start();
         if (isset($_SESSION['user'])) {
